3bfbe1fc4218cacc4272452d000d3b064d93d367
PriscilaIbarra <priscilaibarra2015@hotmail.com>
2023-10-31 08:08:06 -0300

    Auth rules added to json-server fake api routes.

--- VueApp/my-app/server.js
+++ VueApp/my-app/server.js
@@ -2,9 +2,26 @@ const jsonserver = require('json-server');
 const auth = require('json-server-auth');
 const server = jsonserver.create();
 const router = jsonserver.router('db.json');
-const middlewares = jsonserver.defaults()
+const middlewares = jsonserver.defaults();
 
-server.user(auth);
+const cors = require('cors');
+
+server.db = router.db;
+
+const corsConfiguration = {
+    origin: '*'
+} 
+
+const rules = auth.rewriter({
+    users: 640,
+    events: 640,
+    messages: 640,
+    logout: 640
+});
+
+server.use(rules);
+server.use(cors(corsConfiguration));
+server.use(auth);
 server.use(router);
 server.use(middlewares);
-server.listen(3000, () => console.log('Server is listening on PORT 3000'))
+server.listen(process.env.PORT || 3000, () => console.log('Server is listening on PORT 3000'))
--- VueApp/my-app/src/api/api.js
+++ VueApp/my-app/src/api/api.js
@@ -3,7 +3,7 @@ import normalizer from './normalizer'
 import store from '../store';
 
 const axiosConfig = axios.create({
-    baseURL:'https://json-server-vue-app.herokuapp.com',
+    baseURL:'https://json-server-calendar-app-e07dbd917a69.herokuapp.com',
     withCredentials: false,
     headers: {
     'Access-Control-Allow-Origin' : '*',
@@ -21,8 +21,8 @@ export default {
         }
     }
     ,
-    async registerUser(user)
-    {
+    async registerUser(user) 
+    {  
         try
         { 
           const response = await axiosConfig.post('/register',user);  
